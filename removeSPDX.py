ceb8b733aac7248209a1395d45bc485c9a58f1e8
captian210 <captian0210@gmail.com>
2022-01-26 16:43:04 +0300

    removeSPDX script

---
+++ removeSPDX.py
@@ -0,0 +1,24 @@
+# until hardhat flatten tast removes extra SPDX identfiers this is my quick and ditry solution
+import os
+
+bad_words = ['SPDX', 'pragma']
+
+with open('contracts/flatten.sol') as oldfile, open('contracts/flat.sol', 'w') as newfile:
+    for line in oldfile:
+        if not any(bad_word in line for bad_word in bad_words):
+            newfile.write(line)
+            
+filename = "contracts/flat.sol"
+
+string = "// SPDX-License-Identifier: MIT \n pragma solidity ^0.8.0; \n"
+            
+def insert(originalfile,string):
+    with open(originalfile,'r') as f:
+        with open('newfile.txt','w') as f2: 
+            f2.write(string)
+            f2.write(f.read())
+    os.rename('newfile.txt',originalfile)
+    
+insert(filename,string)
+
+os.remove("contracts/flatten.sol")
